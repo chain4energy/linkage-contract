e22014cbe8b50f57c43ec0a1c5d00dab41ee16c7
Maxime Schulz <maxime.schulz@ovoo.pl>
2025-03-10 09:08:10 +0100

    linkage e2e happy case scenario

--- Makefile
+++ Makefile
@@ -17,7 +17,17 @@ optimize:
 	--mount type=volume,source="empty-contract_cache",target=/target \
 	--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
 	cosmwasm/optimizer:0.16.0
+	sudo chown -R $$(id -u):$$(id -g) ./artifacts
 
+
+optimize-nft-contract:
+	@echo "!!!!!!! NOTE: for production use only intel porcessor, so no Mac M1 - see https://github.com/CosmWasm/optimizer"
+# CosmWasm Rust Optimizer
+	cd ./../../external/cw-nfts && docker run  --rm -v .:/code \
+ 	 --mount type=volume,source="empty-contract_cache",target=/target \
+		--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+				cosmwasm/optimizer:0.16.0 && cd -
+	sudo chown -R $$(id -u):$$(id -g) ./../../external/cw-nfts/artifacts
 
 # --user $$(id -u):$$(id -g)
 # docker volume rm empty-contract_cache
--- src/e2e_test/tests.rs
+++ src/e2e_test/tests.rs
@@ -2,33 +2,26 @@
 use std::collections::HashMap;
 
 use cosmrs::{crypto::secp256k1::SigningKey, proto::cosmos::bank::v1beta1::QueryBalanceResponse};
-use cosmwasm_std::{Addr, Binary, Coin, Decimal};
+use cosmwasm_std::{to_base64, Addr, Binary, Coin, Decimal};
+use cw721::{Cw721, Cw721ExecuteMsg, Cw721Query, Cw721QueryMsg};
+use cw721_base::{ExecuteMsg, QueryMsg};
 use serde_json::json;
 use serial_test::serial;
 use e2e_test_suite::{derive_private_key_from_mnemonic, error::CosmError, ContractInit, ADDR_PREFIX};
 
 use crate::{responses::NftLockEntryResponse, state::NftLockEntry};
 
-// use crate::state::{Escrow, EscrowState, LoadedCoins};
-
-// use crate::state::DidDocument;
 
 const CONTRACT_CREATOR_MNEMONIC: &str = "harbor flee number sibling doll recycle brisk mask blanket orphan initial maze race flash limb sound wing ramp proud battle feature ceiling feel miss";
 const HD_PATH: &str = "m/44'/118'/0'/0/0";
-
-// const ESCROW_CONTRACT_NAME: &str = "escrow";
-// // const CONTRACT_PATH: &str = "./artifacts/escrow_contract.wasm";
-// const ESCROW_CONTRACT_PATH: &str = "./target/wasm32-unknown-unknown/release/escrow_contract.wasm";
-
-const DID_CONTRACT_NAME: &str = "did";
-// const DID_CONTRACT_PATH: &str = "./../did/artifacts/did_contract.wasm";
-const DID_CONTRACT_PATH: &str = "./../did/target/wasm32-unknown-unknown/release/did_contract.wasm";
 
 const LINKAGE_CONTRACT_NAME: &str = "linkage";
 const LINKAGE_CONTRACT_PATH: &str = "./target/wasm32-unknown-unknown/release/linkage_contract.wasm";
 
 const CW721_BASE_CONTRACT_NAME: &str = "cw721_base";
-const CW721_BASE_CONTRACT_PATH: &str = "./../../external/cw-nfts/target/wasm32-unknown-unknown/release/cw721_base.wasm";
+// const CW721_BASE_CONTRACT_PATH: &str = "./../../external/cw-nfts/target/wasm32-unknown-unknown/release/cw721_base.wasm";
+const CW721_BASE_CONTRACT_PATH: &str = "./../../external/cw-nfts/artifacts/cw721_base.wasm";
+
 
 
 
@@ -81,17 +74,78 @@ fn test_full_linkage_process() {
 
     let context = e2e_test_suite::get_context();
 
-    let linkage_contract_address = context.get_contracts_info().get(LINKAGE_CONTRACT_NAME).expect("no linkage-contract contract info").contract_address.clone();
+    let cw721_base_contract_address = context.get_contracts_info().get(CW721_BASE_CONTRACT_NAME).expect("no cw721_base contract info").contract_address.clone();
+    let linkage_contract_address = context.get_contracts_info().get(LINKAGE_CONTRACT_NAME).expect("no linkage contract info").contract_address.clone();
     let (contract_admin_key, contract_admin_address) = create_key_and_address();
-    let token_id = "NFT1".to_string();
+    let token_id = "C4E1".to_string();
 
-    // ------ Receive (lock) NFT
+    let did = "did_1233";
+    let base64_encoded = to_base64(did);
+    println!("Base64 Encoded: {}", base64_encoded);
+    // let did_binary = cosmwasm_std::Binary::from_base64(&base64_encoded).expect("Base64 decode failed");
+    // println!("Binary: {:?}", did_binary);
 
-    let exec_msg = super::super::contract::sv::ExecMsg::ReceiveNft { sender: Addr::unchecked(contract_admin_address.clone()), token_id: token_id.clone(), msg: Binary::from_base64("ZGlkXzEyMzM=").expect("Base64 decode failed")};
+
+    // ------ Mint NFT in cw721_base contract
+
+    let exec_msg: ExecuteMsg<(), ()> = cw721_base::msg::ExecuteMsg::Mint { token_id: token_id.clone(), owner: contract_admin_address.clone(), extension: (), token_uri: None };
+    let msg = json!(exec_msg).to_string();
+    let result = context.get_chain_client().tx.wasm().execute_contract_msg(&contract_admin_key, &cw721_base_contract_address.clone(), &msg, vec![]);
+    assert!(result.is_ok(), "Expected OK, but go an Err");
+
+
+    // ------ Check owner of NFT in cw721_base contract
+
+    let exec_msg: QueryMsg<()> = cw721_base::msg::QueryMsg::OwnerOf { token_id: token_id.clone(), include_expired: None };
     let msg = json!(exec_msg).to_string();
 
-    let result = context.get_chain_client().tx.wasm().execute_contract_msg(&contract_admin_key, &linkage_contract_address, &msg, vec![]);
-    assert!(result.is_ok(), "Expected OK, but got an Err");
+    let result = context.get_chain_client().query.wasm().contract(&cw721_base_contract_address.clone(), &msg);
+
+    assert!(result.is_ok(), "Expected OK, but go an Err");
+    let result = result.unwrap();
+    let resp = String::from_utf8(result.clone().data).expect("Invalid UTF-8 sequence");
+    println!("Cw721 owner of resp: {resp}");
+
+    let owner_of: cw721::OwnerOfResponse = serde_json::from_slice(&result.data).expect("Get owner of response deserialization error");
+    let expected_owner_of = cw721::OwnerOfResponse {
+        approvals: vec![],
+        owner: contract_admin_address.clone()
+    };
+
+    assert_eq!(expected_owner_of, owner_of);
+
+
+    // ------ Send NFT from cw721 to linkage   
+
+    // TODO: check coswasm_std binary type conflict between linkage and cw721_base
+    let msg = format!(r#"{{"send_nft":{{"contract":"{}", "token_id":"{}", "msg":"{}"}}}}"#, linkage_contract_address.clone(), token_id.clone(), base64_encoded.to_string());
+    // let exec_msg: ExecuteMsg<(), ()> = cw721_base::msg::ExecuteMsg::SendNft { contract: linkage_contract_address.clone(), token_id: token_id.clone(), msg: did_binary };
+    let result = context.get_chain_client().tx.wasm().execute_contract_msg(&contract_admin_key, &cw721_base_contract_address.clone(), &msg, vec![]);
+    assert!(result.is_ok(), "Expected OK, but go an Err");
+    let result = result.unwrap();
+    let resp = String::from_utf8(result.clone().data).expect("Invalid UTF-8 sequence");
+    println!("Send NFT resp: {resp}");
+
+
+    // ------ Check owner after transfering NFT to linkage
+    
+    let exec_msg: QueryMsg<()> = cw721_base::msg::QueryMsg::OwnerOf { token_id: token_id.clone(), include_expired: None };
+    let msg = json!(exec_msg).to_string();
+
+    let result = context.get_chain_client().query.wasm().contract(&cw721_base_contract_address.clone(), &msg);
+
+    assert!(result.is_ok(), "Expected OK, but go an Err");
+    let result = result.unwrap();
+    let resp = String::from_utf8(result.clone().data).expect("Invalid UTF-8 sequence");
+    println!("Cw721 owner of resp: {resp}");
+
+    let owner_of: cw721::OwnerOfResponse = serde_json::from_slice(&result.data).expect("Get owner of response deserialization error");
+    let expected_owner_of = cw721::OwnerOfResponse {
+        approvals: vec![],
+        owner: linkage_contract_address.clone()
+    };
+
+    assert_eq!(expected_owner_of, owner_of);
 
 
     // ------ Get authorized contracts
@@ -106,14 +160,14 @@ fn test_full_linkage_process() {
     let resp = String::from_utf8(result.clone().data).expect("Invalid UTF-8 sequence");
     println!("Authorized contracts: {resp}");
     let authorized_contracts: Vec<Addr> = serde_json::from_slice(&result.data).expect("Get Authorized Contracts response deserialization error");
-    let expected_authorized_contracts = vec![Addr::unchecked(contract_admin_address.clone())];
+    let expected_authorized_contracts = vec![Addr::unchecked(cw721_base_contract_address.clone())];
 
     assert_eq!(expected_authorized_contracts.clone(), authorized_contracts);
 
 
     // ------ Get locked NFT by token_id
 
-    let query_msg = super::super::contract::sv::QueryMsg::GetLockedNft { contract_address: Addr::unchecked(contract_admin_address.clone()), token_id: token_id.clone() };
+    let query_msg = super::super::contract::sv::QueryMsg::GetLockedNft { contract_address: Addr::unchecked(cw721_base_contract_address.clone()), token_id: token_id.clone() };
     let msg = json!(query_msg).to_string();
     let result = context.get_chain_client().query.wasm().contract(&linkage_contract_address.clone(), &msg);
     assert!(result.is_ok(), "Expected OK, but got an Err");
@@ -125,7 +179,7 @@ fn test_full_linkage_process() {
     let expected_nft_lock_entry = NftLockEntryResponse{ 
         did: "did_1233".to_string(),
         sender: Addr::unchecked(contract_admin_address.clone()),
-        contract_address: Addr::unchecked(contract_admin_address.clone()),
+        contract_address: Addr::unchecked(cw721_base_contract_address.clone()),
         token_id: token_id.clone()
     };
 
@@ -146,7 +200,7 @@ fn test_full_linkage_process() {
     let expected_nft_lock_entry = vec![NftLockEntryResponse{ 
         did: "did_1233".to_string(),
         sender: Addr::unchecked(contract_admin_address.clone()),
-        contract_address: Addr::unchecked(contract_admin_address.clone()),
+        contract_address: Addr::unchecked(cw721_base_contract_address.clone()),
         token_id: token_id.clone()
     }];
 
@@ -167,14 +221,38 @@ fn test_full_linkage_process() {
     let expected_nft_lock_entry = vec![NftLockEntryResponse{ 
         did: "did_1233".to_string(),
         sender: Addr::unchecked(contract_admin_address.clone()),
-        contract_address: Addr::unchecked(contract_admin_address),
+        contract_address: Addr::unchecked(cw721_base_contract_address.clone()),
         token_id: token_id.clone()
     }];
 
     assert_eq!(expected_nft_lock_entry.clone(), nft_lock_entry);
 
-    // ------ Send (unlock) NFT
+    
+    // ------ Unlock NFT
+    
+    let query_msg = format!(r#"{{"unlock_nft":{{"contract_address":"{}", "token_id":"{}"}}}}"#, cw721_base_contract_address.clone(), token_id.clone());
+    let result = context.get_chain_client().tx.wasm().execute_contract_msg(&contract_admin_key, &linkage_contract_address.clone(), &query_msg, vec![]);
+    assert!(result.is_ok(), "Expected OK, but go an Err");
+    
+    // ------ Last NFT owner check in cw721_base contract
 
+    let exec_msg: QueryMsg<()> = cw721_base::msg::QueryMsg::OwnerOf { token_id: token_id.clone(), include_expired: None };
+    let msg = json!(exec_msg).to_string();
+   
+    let result = context.get_chain_client().query.wasm().contract(&cw721_base_contract_address.clone(), &msg);
+   
+    assert!(result.is_ok(), "Expected OK, but go an Err");
+    let result = result.unwrap();
+    let resp = String::from_utf8(result.clone().data).expect("Invalid UTF-8 sequence");
+    println!("Cw721 owner of resp: {resp}");
+   
+    let owner_of: cw721::OwnerOfResponse = serde_json::from_slice(&result.data).expect("Get owner of response deserialization error");
+    let expected_owner_of = cw721::OwnerOfResponse {
+        approvals: vec![],
+        owner: contract_admin_address.clone()
+    };
+   
+    assert_eq!(expected_owner_of, owner_of);
 
 
 }
@@ -205,54 +283,25 @@ fn init_suite() {
 
     let mut contracts: HashMap<String, ContractInit> = HashMap::new();
     // contracts.insert(LINKAGE_CONTRACT_NAME.into(), ContractInit { contract_path: LINKAGE_CONTRACT_PATH.to_string(), json_ncoded_init_args: format!(r#"{{"admins": ["{}"], "authorized_nft_contracts": ["{}"]}}"#, &owner_addr, &owner_addr), label: "linkage_contract".to_string() });
-    contracts.insert(CW721_BASE_CONTRACT_NAME.into(), ContractInit { contract_path: CW721_BASE_CONTRACT_PATH.to_string(), json_ncoded_init_args: format!(r#"{{"admins": ["{}"]}}"#, &owner_addr), label: "cw721_contract".to_string() });
+    contracts.insert(CW721_BASE_CONTRACT_NAME.into(), ContractInit { contract_path: CW721_BASE_CONTRACT_PATH.to_string(), json_ncoded_init_args: format!(r#"{{"name":"{}", "symbol":"{}"}}"#, "C4E_NFT_COLLECTION", "C4E"), label: "cw721_contract".to_string() });
 
     e2e_test_suite::init_suite(CONTRACT_CREATOR_MNEMONIC, HD_PATH, &contracts, "c4e-chain-linkage:v1.4.4", "linkage-contract", "linkage");
 
-    // let context = e2e_test_suite::get_context();
+    let cw721_base_contract_address: String;
+    {
+        let context = e2e_test_suite::get_context();
+        cw721_base_contract_address = context.get_contracts_info().get(CW721_BASE_CONTRACT_NAME).expect("no cw721-base contract info").contract_address.clone();
+    }
 
-    // let mut cw721_base_contract_address: String;
-    // {
-    //     let context = e2e_test_suite::get_context();
-    //     cw721_base_contract_address = context.get_contracts_info().get(CW721_BASE_CONTRACT_NAME).expect("no cw721-base contract info").contract_address.clone();
-    // }
-
-    // e2e_test_suite::add_contract(CONTRACT_CREATOR_MNEMONIC, HD_PATH, LINKAGE_CONTRACT_NAME, 
-    //     ContractInit {
-    //         contract_path: LINKAGE_CONTRACT_PATH.to_string(), 
-    //         json_ncoded_init_args: format!(r#"{{"admins": ["{}"], "authorized_nft_contracts": ["{}"]}}"#, &owner_addr, cw721_base_contract_address), 
-    //         label: "linkage_contract".to_string()
-    //     }
-    // );
+    e2e_test_suite::add_contract(CONTRACT_CREATOR_MNEMONIC, HD_PATH, LINKAGE_CONTRACT_NAME, 
+        ContractInit {
+            contract_path: LINKAGE_CONTRACT_PATH.to_string(), 
+            json_ncoded_init_args: format!(r#"{{"admins": ["{}"], "authorized_nft_contracts": ["{}"]}}"#, &owner_addr, cw721_base_contract_address), 
+            label: "linkage_contract".to_string()
+        }
+    );
     // let context: std::sync::RwLockReadGuard<'_, e2e_test_suite::TestSuiteContextInternal> = e2e_test_suite::get_context();
 }
-
-// fn init_suite() {
-//     let (owner_key, owner_addr) = create_key_and_address();
-
-//     let mut contracts: HashMap<String, ContractInit> = HashMap::new();
-//     contracts.insert(LINKAGE_CONTRACT_NAME.into(), ContractInit { contract_path: LINKAGE_CONTRACT_PATH.to_string(), json_ncoded_init_args: format!(r#"{{"admins": ["{}"], "authorized_nft_contracts": ["{}"]}}"#, &owner_addr, &owner_addr), label: "linkage_contract".to_string() });
-//     // contracts.insert(CW721_BASE_CONTRACT_NAME.into(), ContractInit { contract_path: CW721_BASE_CONTRACT_PATH.to_string(), json_ncoded_init_args: format!(r#"{{"admins": ["{}"]}}"#, &owner_addr), label: "cw721_base".to_string() });
-
-//     e2e_test_suite::init_suite(CONTRACT_CREATOR_MNEMONIC, HD_PATH, &contracts, "c4e-chain-linkage:v1.4.4", "linkage-contract", "linkage");
-
-//     // let context = e2e_test_suite::get_context();
-
-//     // let mut cw721_base_contract_address: String;
-//     // {
-//     //     let context = e2e_test_suite::get_context();
-//     //     cw721_base_contract_address = context.get_contracts_info().get(CW721_BASE_CONTRACT_NAME).expect("no cw721-base contract info").contract_address.clone();
-//     // }
-
-//     // e2e_test_suite::add_contract(CONTRACT_CREATOR_MNEMONIC, HD_PATH, LINKAGE_CONTRACT_NAME, 
-//     //     ContractInit {
-//     //         contract_path: LINKAGE_CONTRACT_PATH.to_string(), 
-//     //         json_ncoded_init_args: format!(r#"{{"admins": ["{}"], "authorized_nft_contracts": ["{}"]}}"#, &owner_addr, cw721_base_contract_address), 
-//     //         label: "linkage_contract".to_string()
-//     //     }
-//     // );
-//     // let context: std::sync::RwLockReadGuard<'_, e2e_test_suite::TestSuiteContextInternal> = e2e_test_suite::get_context();
-// }
 
 fn create_key_and_address() -> (SigningKey, String){
     create_key_and_address_from_mnemonic(CONTRACT_CREATOR_MNEMONIC)
